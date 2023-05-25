2803693f78378888eebad7fab4bbc182ef545d35
Volodymyr Zhukoskyi <044.zhukovskyi.volodymyr@gmail.com>
2023-05-25 14:29:39 +0300

    homeWork1

---
+++ index.js
@@ -0,0 +1,8 @@
+const helloUser = prompt("User, what is your name?")
+if (helloUser === "John" || helloUser === "john"){
+  alert("Hello, John! How are you?")
+}
+else{
+  alert("Hello, noname! How are you?")
+}
+
